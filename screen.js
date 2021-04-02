242497e43ddaff0afc1e6448839bbf090a482b2d
James McGlone <jamespmcglone@gmail.com>
2021-04-02 09:40:59 +0800

    Trying old commit

--- screen.js
+++ screen.js
@@ -1,6 +1,5 @@
 // required for the kill process
 var psTree = require('ps-tree');
-
 var io = require('socket.io-client');
 
 const socket = io("wss://bs-pager.herokuapp.com")
@@ -9,10 +8,14 @@ const socket = io("wss://bs-pager.herokuapp.com")
 let state = {
   // currentRoom: '',
   // roomStatus: ''
-  bigString: 'sudo ../text-scroller -f ../../fonts/nethack16.bdf --led-chain=8  --led-rows=16 --led-cols=8 --led-multiplexing=18 --led-parallel=2 --led-slowdown-gpio=5 --led-brightness=100 --led-multiplexing=18 --led-pixel-mapper=Flipper -s.5 -C0,20,255 -t-2 '
+  bigString: `sudo ../text-scroller -f ../../fonts/nethack16.bdf --led-chain=8  --led-rows=16 --led-cols=8 --led-multiplexing=18 --led-parallel=2 --led-slowdown-gpio=5 --led-brightness=100 --led-multiplexing=18 --led-pixel-mapper=Flipper -s.5 -C0,20,255 -t-2 lalalaalalalalalalalalalala `
 }
 
 
+
+
+// running process
+let runner
 
 
 function startMessage (message) {
@@ -20,10 +23,7 @@ function startMessage (message) {
   // state.bigString + message
   // var command="echo '<password>' | sudo -S '<command that needs a root access>'";
   var exec = require('child_process').exec;
-  // let runner = 
-  // exec('pkill -f text-scroller')
-
-  exec(state.bigString + message, function(error, stdout, stderr) {
+  runner = exec(state.bigString, function(error, stdout, stderr) {
       console.log('stdout: ' + stdout)
       // console.log('stderr: ' + stderr);
       if (error !== null) {
@@ -32,68 +32,34 @@ function startMessage (message) {
   })
 }
 
-function simleStart (message) {
+function stopMessage() {
   
-
-  // state.bigString + message
-  // var command="echo '<password>' | sudo -S '<command that needs a root access>'";
-  
-  // let runner = 
-  
-  // exec('sudo killall -9 text-scroller', function(error, stdout, stderr) {
-  //     console.log('stdout: ' + stdout)
-  //     // console.log('stderr: ' + stderr);
-  //     if (error !== null) {
-  //         console.log('exec error: ' + error)
-  //     }
-  // })
-
-
-
-//   setTimeout(() => {
-  // killProcess('text-scroller');    
-  
-  var exec = require('child_process').exec;
-  exec('hello', function(error, stdout, stderr) {
-      console.log('stdout: ' + stdout)
-      // console.log('stderr: ' + stderr);
-      if (error !== null) {
-          console.log('exec error: ' + error)
+  // // killing process
+  var kill = function (pid, signal, callback) {
+      signal   = signal || 'SIGKILL';
+      callback = callback || function () {}
+      var killTree = true;
+      if(killTree) {
+          psTree(pid, function (err, children) {
+              [pid].concat(
+                  children.map(function (p) {
+                      return p.PID;
+                  })
+              ).forEach(function (tpid) {
+                  try { process.kill(tpid, signal) }
+                  catch (ex) { }
+              });
+              callback();
+          });
+      } else {
+          try { process.kill(pid, signal) }
+          catch (ex) { }
+          callback()
       }
-  })
-
-// }, 5000);
+  };
+  kill(runner.pid);
 
 }
-
-// function stopMessage() {
-  
-//   // // killing process
-//   var kill = function (pid, signal, callback) {
-//       signal   = signal || 'SIGKILL';
-//       callback = callback || function () {}
-//       var killTree = true;
-//       if(killTree) {
-//           psTree(pid, function (err, children) {
-//               [pid].concat(
-//                   children.map(function (p) {
-//                       return p.PID;
-//                   })
-//               ).forEach(function (tpid) {
-//                   try { process.kill(tpid, signal) }
-//                   catch (ex) { }
-//               });
-//               callback();
-//           });
-//       } else {
-//           try { process.kill(pid, signal) }
-//           catch (ex) { }
-//           callback()
-//       }
-//   };
-//   kill(runner.pid);
-
-// }
 
 
 
@@ -101,16 +67,25 @@ socket.on('connect', function(socketId) {
   
   console.log('connected to server')
   socket.emit('screenConnect', 'create');
-  // startMessage('init')
+  startMessage('inittttttttttttt')
 
+//   socket.on('room', (data) => {
+//     state.currentRoom = data.roomName
+//     console.log('room message...')
+//     console.log('state: ' + JSON.stringify(state, null, 4))
+//  })
 
+//  socket.on('roomStatus', (roomStatus) => {
+//      state.roomStatus = roomStatus;
+//      console.log('roomStatus message...')
+//      console.log('state: ' + JSON.stringify(state, null, 4))
+//  })
 
  socket.on('startMessage', (data) => {
   console.log('start the message')
   console.log(JSON.stringify(data))
-  // stopMessage()
-  //  startMessage(data.message)
-  //  startMessage('aaaaaaaaaa')
+  stopMessage()
+   startMessage(data.message)
  })
 
 //  socket.on('stopMessage', (data) => {
