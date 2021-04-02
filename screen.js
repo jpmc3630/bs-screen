edce4a4734653c36efd2e2e27c4ddc0e7b40e362
James McGlone <jamespmcglone@gmail.com>
2021-04-03 02:38:09 +0800

    Set log paths

--- screen.js
+++ screen.js
@@ -2,7 +2,7 @@
 // create a rolling file logger based on date/time that fires process events
 const opts = {
   errorEventName:'error',
-      logDirectory:'/logs', // NOTE: folder must exist and be writable...
+      logDirectory:'logs', // NOTE: folder must exist and be writable...
       fileNamePattern:'roll-<DATE>.log',
       dateFormat:'YYYY.MM.DD'
 };
