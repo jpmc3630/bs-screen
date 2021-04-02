1ca8baf637083443c73d9972e063aa358437386b
James McGlone <jamespmcglone@gmail.com>
2021-04-02 13:31:54 +0800

    asdasd

--- screen.js
+++ screen.js
@@ -57,10 +57,34 @@ function startMessage (message) {
 
   setTimeout(function() {
 
-  child_process.execFile('closeme.sh', [runner.pid], function(error, stdout, stderr){
-    console.log(stdout);
-  });
-  
+  // child_process.execFile('closeme.sh', [runner.pid], function(error, stdout, stderr){
+  //   console.log(stdout);
+  // });
+    // // killing process
+    var kill = function (pid, signal, callback) {
+      signal   = signal || 'SIGKILL';
+      callback = callback || function () {}
+      var killTree = true;
+      if(killTree) {
+          psTree(pid, function (err, children) {
+              [pid].concat(
+                  children.map(function (p) {
+                      return p.PID;
+                  })
+              ).forEach(function (tpid) {
+                  try { process.kill(tpid, signal) }
+                  catch (ex) { }
+              });
+              callback();
+          });
+      } else {
+          try { process.kill(pid, signal) }
+          catch (ex) { }
+          callback()
+      }
+  };
+  console.log(runner.pid)
+  kill(runner.pid);
 
   }, 7000);
 
