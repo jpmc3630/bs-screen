dd1f5c05a26386e1c183c12a5eaa90ffbfdc3a2d
James McGlone <jamespmcglone@gmail.com>
2021-04-03 03:49:33 +0800

    remove logging

--- screen.js
+++ screen.js
@@ -1,13 +1,13 @@
 
 // create a rolling file logger based on date/time that fires process events
-const opts = {
-  errorEventName:'error',
-      logDirectory:'logs',
-      fileNamePattern:'roll-<DATE>.log',
-      dateFormat:'YYYY.MM.DD'
-};
+// const opts = {
+//   errorEventName:'error',
+//       logDirectory:'logs',
+//       fileNamePattern:'roll-<DATE>.log',
+//       dateFormat:'YYYY.MM.DD'
+// };
 
-const log = require('simple-node-logger').createRollingFileLogger( opts );
+// const log = require('simple-node-logger').createRollingFileLogger( opts );
 
 
 var io = require('socket.io-client');
@@ -25,7 +25,7 @@ let state = {
 
 function startScreen(message, color, colorOutline, bgColor, speed, spacing) {
   console.log(socket.id)
-  log.warn(socket.id);
+  // log.warn(socket.id);
   
   state.running_pids.forEach(element => {
     kill(element)
@@ -72,7 +72,7 @@ function startScreen(message, color, colorOutline, bgColor, speed, spacing) {
   })
   state.running_pids.push(runner.pid)
   console.log(state.running_pids)
-  log.warn(state.running_pids);
+  // log.warn(state.running_pids);
 
 
   }, 2000);
@@ -82,18 +82,18 @@ function startScreen(message, color, colorOutline, bgColor, speed, spacing) {
 // 
 socket.io.on("reconnect", () => {
   console.log('reconnected')
-  log.warn('reconnected');
+  // log.warn('reconnected');
 });
 
 socket.on("connect_error", () => {
-  log.warn('connection error ... trying to reconnect ...');
+  // log.warn('connection error ... trying to reconnect ...');
   console.log('connection error ... trying to reconnect....')
   socket.connect();
 });
 
 socket.on('connect', function(socketId) {
   
-  log.warn('connected to server');
+  // log.warn('connected to server');
   console.log('connected to server')
   socket.emit('screenConnect', 'create');
   startScreen('init patch', '180,50,80', '70,100,160', '0,0,0', '0.3','-1')
@@ -101,7 +101,7 @@ socket.on('connect', function(socketId) {
 
 
  socket.on('startMessage', function(data) {
-  log.info(data[0]);
+  // log.info(data[0]);
   console.log(data[0])
   console.log(data[1].join())
   console.log(data[2].join())
