b8075cfcca5a43f1784320fcae1290502fa5807d
James McGlone <jamespmcglone@gmail.com>
2021-04-02 21:36:08 +0800

    asdasd

--- screen.js
+++ screen.js
@@ -138,13 +138,13 @@ socket.on('connect', function(socketId) {
 
 
 
- socket.on('startMessage', function({ message, color }) {
+ socket.on('startMessage', function(data) {
   // console.log('start the message')
   
-  // console.log(JSON.stringify(data))
+  console.log(JSON.stringify(data, null, 4))
 
-  console.log('message: ' + message)
-  console.log('color: ' + color)
+  // console.log('message: ' + message)
+  // console.log('color: ' + color)
 
   // console.log(JSON.stringify(data))
   // console.log(data.message)
