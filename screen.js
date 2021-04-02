1bf84f01c1c61c69f4c7d42c6bdc0d082075dea3
James McGlone <jamespmcglone@gmail.com>
2021-04-02 12:32:45 +0800

    asd

--- screen.js
+++ screen.js
@@ -34,7 +34,7 @@ function startMessage (message) {
   
 
   //  = spawn('seq', '', {detached: true});
-  var child=spawn('sex', ['10000000000'], function(){console.log('end');}, {timeout:6000});
+  var child=spawn('sex', ['10000000000']);
 
 // var exec = require('child_process').exec;
 //    let runner = exec(state.bigString, function(error, stdout, stderr) {
