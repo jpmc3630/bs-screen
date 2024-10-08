a1786b6be17598f60cca9f3915e42e254ad0e2e2
James McGlone <james@anyway.software>
2024-10-08 12:18:15 +0800

    remove default trigger

--- screen.js
+++ screen.js
@@ -111,7 +111,10 @@ function startScreen(message, color, colorOutline, bgColor, speed, spacing) {
 //   socket.connect();
 // });
 
-startScreen('...........', '180,50,80', '70,100,160', '0,0,0', '0.3','-1')
+
+
+
+// startScreen('...........', '180,50,80', '70,100,160', '0,0,0', '0.3','-1')
 
 socket.on('connect', function(socketId) {
   
