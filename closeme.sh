2c524e7724b65f2203fa33246dad0a18b1a1f526
James McGlone <james@anyway.software>
2024-10-09 17:43:03 +0800

    Revert "revert kill me script"
    This reverts commit b9aef59dd56fc382146918f5dabfe9a889a867fc.

--- closeme.sh
+++ closeme.sh
@@ -10,17 +10,17 @@
 
 
 # original:
-kill -9 `ps aux | grep text | awk '{print $2}'`
+# kill -9 `ps aux | grep text | awk '{print $2}'`
 
-kill -9 $(pidof text-scroller)
-kill -9 $(pidof text-scro)
+# kill -9 $(pidof text-scroller)
+# kill -9 $(pidof text-scro)
 
 # updated with texture:
 
 # Kill processes matching "text" from ps and led-image-viewer
-# kill -9 $(ps aux | grep text | grep -v grep | awk '{print $2}')
-# kill -9 $(ps aux | grep led-image-viewer | grep -v grep | awk '{print $2}')
+kill -9 $(ps aux | grep text | grep -v grep | awk '{print $2}')
+kill -9 $(ps aux | grep led-image-viewer | grep -v grep | awk '{print $2}')
 
-# # Kill processes specifically named "text-scroller" and "led-image-viewer"
-# kill -9 $(pidof text-scroller)
-# kill -9 $(pidof led-image-viewer)
+# Kill processes specifically named "text-scroller" and "led-image-viewer"
+kill -9 $(pidof text-scroller)
+kill -9 $(pidof led-image-viewer)
